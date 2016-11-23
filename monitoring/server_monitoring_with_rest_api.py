5341f4557c6950ecce58b284f538bfb9ba7dd520
Illusion <gunboy83@gmail.com>
2016-11-23 19:59:05 +0900

    Server monitoring source initial commit

--- monitoring/server_monitoring_with_rest_api.py
+++ monitoring/server_monitoring_with_rest_api.py
@@ -1,11 +1,14 @@
 import time
 import os, subprocess
 
-rest_api_address = 'http://vincent.nhnent.com:8979/hand'
+#rest_api_address = 'http://vincent.nhnent.com:8979/hand'
+rest_api_address = '10.165.128.51:8979/hand'
 content_type = 'Content-type: application/octet-stream'
 img_path = '/Users/Illusion/Documents/Data/palm_data/test_set/crop_resize_512_512'
 
 os.chdir(img_path)
 
-file_string = '@' + "wr.jpg"
-subprocess.call( ["curl", '-H', content_type, rest_api_address, '--data-binary', file_string] )
+while True:
+    file_string = '@' + "wr.jpg"
+    subprocess.call( ["curl", '-H', content_type, rest_api_address, '--data-binary', file_string] )
+    time.sleep(0.2)

f92fa72d6514d5f90a76c8fb54405cebaf1c7157
TheIllusion <gunboy83@gmail.com>
2016-12-18 00:36:26 +0900

    add trn001 and svc002 to 'fortune_server_data_management' logic

--- fortune_server_data_management/get_data_from_svc.sh
+++ fortune_server_data_management/get_data_from_svc.sh
@@ -5,12 +5,18 @@ if [ $# -eq 0 ]
     echo "No arguments supplied"
 else
 	date=$1
-	wget http://10.161.31.22:8985/server_hand_data/$date.tar
+    wget http://10.161.31.22:8985/server_hand_data/$date.tar
     tar -xvf $date.tar
     mv $date.tar $date.svc001.tar
     wget http://10.161.31.24:8985/server_hand_data/$date.tar
     tar -xvf $date.tar
     mv $date.tar $date.svc003.tar
+    wget http://10.161.31.25:8985/server_hand_data/$date.tar
+    tar -xvf $date.tar
+    mv $date.tar $date.trn001.tar
+    wget http://10.161.31.23:8985/server_hand_data/$date.tar
+    tar -xvf $date.tar
+    mv $date.tar $date.svc002.tar
     cd $date
     find ./* -size -92k -size +90k -exec rm -rf {} \;
     cd /home/nhnent/H1/users/rklee/palmdemo_flask_dev

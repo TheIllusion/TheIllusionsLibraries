d2b2eada760966fe626d385a9b1fa32414cf82ea
Illusion <gunboy83@gmail.com>
2016-12-20 19:09:36 +0900

    Add Try and Except

--- monitoring/urgent_monitoring_with_rest_api.py
+++ monitoring/urgent_monitoring_with_rest_api.py
@@ -4,7 +4,7 @@ import urllib2
 import datetime
 import re
 
-REQUEST_SENDING_INTERVAL = 30
+REQUEST_SENDING_INTERVAL = 60
 REQUEST_TIMEOUT_THRESHOLD_SEC = 30
 RESPONSE_TIME_WARINING_THRESHOLD = 5
 
@@ -54,51 +54,63 @@ while True:
 
     print '==================================================================='
     print 'hand ip = ', hand_rest_api_address[index]
-    req = urllib2.Request(hand_rest_api_address[index], data = hand_image)
-    req.add_header('Content-Length', '%d' % os.path.getsize(hand_img_path))
-    req.add_header('Content-Type', 'application/octet-stream')
 
-    start_time = time.time()
+    try:
+        start_time = time.time()
 
-    res = urllib2.urlopen(req, timeout=REQUEST_TIMEOUT_THRESHOLD_SEC).read().strip()
+        req = urllib2.Request(hand_rest_api_address[index], data=hand_image)
+        req.add_header('Content-Length', '%d' % os.path.getsize(hand_img_path))
+        req.add_header('Content-Type', 'application/octet-stream')
 
-    elapsed_time = time.time() - start_time
+        res = urllib2.urlopen(req, timeout=REQUEST_TIMEOUT_THRESHOLD_SEC).read().strip()
 
-    current_time = datetime.datetime.strftime(datetime.datetime.now(), '%Y-%m-%d %H:%M:%S')
+        elapsed_time = time.time() - start_time
 
-    line_string = current_time + ' Elapsed time(hand) = ' + str(elapsed_time) + '\n'
-    print line_string
+        current_time = datetime.datetime.strftime(datetime.datetime.now(), '%Y-%m-%d %H:%M:%S')
 
-    all_log_file = open(FILE_PATH_FOR_ALL_LOG, 'a')
-    all_log_file.write(line_string)
-    all_log_file.close()
+        line_string_all_log = current_time + ' Elapsed time(hand) = ' + str(elapsed_time) + '\n'
+        print line_string_all_log
 
-    if elapsed_time > RESPONSE_TIME_WARINING_THRESHOLD:
+        all_log_file = open(FILE_PATH_FOR_ALL_LOG, 'a')
+        all_log_file.write(line_string_all_log)
+        all_log_file.close()
+
+        if elapsed_time > RESPONSE_TIME_WARINING_THRESHOLD:
+            warning_log_file = open(FILE_PATH_FOR_WARNING_LOG, 'a')
+
+            line_string = current_time + ' Warning!! Response time for hand recognition is slower than ' \
+                          + str(RESPONSE_TIME_WARINING_THRESHOLD) + 's. Response Time = ' + str(elapsed_time) + '\n'
+
+            print line_string
+            warning_log_file.write(line_string)
+            warning_log_file.close()
+
+        # Check the validity of the response string
+        match = re.search('"Status": "OK"', res)
+        match_2 = re.search('"Gamjeong": 2001', res)
+        if match and match_2:
+            print "Valid response data"
+        else:
+            print "Invalid response data"
+            warning_log_file = open(FILE_PATH_FOR_WARNING_LOG, 'a')
+
+            line_string = current_time + ' Warning!! Invalid response detected from the hand response' + '\n'
+
+            print line_string
+            warning_log_file.write(line_string)
+            warning_log_file.close()
+
+            # print res
+
+    except:
         warning_log_file = open(FILE_PATH_FOR_WARNING_LOG, 'a')
 
-        line_string = current_time + ' Warning!! Response time for hand recognition is slower than ' \
-                      + str(RESPONSE_TIME_WARINING_THRESHOLD) + 's. Response Time = ' + str(elapsed_time) + '\n'
+        current_time = datetime.datetime.strftime(datetime.datetime.now(), '%Y-%m-%d %H:%M:%S')
+        line_string = current_time + ' Error!! Exception occurred in hand! ' + hand_rest_api_address[index] + '\n'
 
         print line_string
         warning_log_file.write(line_string)
         warning_log_file.close()
-
-    # Check the validity of the response string
-    match = re.search('"Status": "OK"', res)
-    match_2 = re.search('"Gamjeong": 2001', res)
-    if match and match_2:
-        print "Valid response data"
-    else:
-        print "Invalid response data"
-        warning_log_file = open(FILE_PATH_FOR_WARNING_LOG, 'a')
-
-        line_string = current_time + ' Warning!! Invalid response detected from the hand response' + '\n'
-
-        print line_string
-        warning_log_file.write(line_string)
-        warning_log_file.close()
-
-    #print res
 
     time.sleep(REQUEST_SENDING_INTERVAL)
 
@@ -108,53 +120,66 @@ while True:
     print '==================================================================='
     print 'face ip = ', face_rest_api_address[index]
 
-    req = urllib2.Request(face_rest_api_address[index], data=face_image)
-    req.add_header('Content-Length', '%d' % os.path.getsize(face_img_path))
-    req.add_header('Content-Type', 'application/octet-stream')
+    try:
+        req = urllib2.Request(face_rest_api_address[index], data=face_image)
+        req.add_header('Content-Length', '%d' % os.path.getsize(face_img_path))
+        req.add_header('Content-Type', 'application/octet-stream')
 
-    start_time = time.time()
+        start_time = time.time()
 
-    res = urllib2.urlopen(req, timeout=REQUEST_TIMEOUT_THRESHOLD_SEC).read().strip()
+        res = urllib2.urlopen(req, timeout=REQUEST_TIMEOUT_THRESHOLD_SEC).read().strip()
 
-    elapsed_time = time.time() - start_time
+        elapsed_time = time.time() - start_time
 
-    current_time = datetime.datetime.strftime(datetime.datetime.now(), '%Y-%m-%d %H:%M:%S')
+        current_time = datetime.datetime.strftime(datetime.datetime.now(), '%Y-%m-%d %H:%M:%S')
 
-    line_string = current_time + ' Elapsed time(face) = ' + str(elapsed_time) + '\n'
-    print line_string
+        line_string = current_time + ' Elapsed time(face) = ' + str(elapsed_time) + '\n'
+        print line_string
 
-    all_log_file = open(FILE_PATH_FOR_ALL_LOG, 'a')
-    all_log_file.write(line_string)
-    all_log_file.close()
+        all_log_file = open(FILE_PATH_FOR_ALL_LOG, 'a')
+        all_log_file.write(line_string)
+        all_log_file.close()
 
-    if elapsed_time > RESPONSE_TIME_WARINING_THRESHOLD:
+        if elapsed_time > RESPONSE_TIME_WARINING_THRESHOLD:
+            warning_log_file = open(FILE_PATH_FOR_WARNING_LOG, 'a')
+
+            line_string = current_time + ' Warning!! Response time for face recognition is slower than ' \
+                          + str(RESPONSE_TIME_WARINING_THRESHOLD) + 's. Response Time = ' + str(elapsed_time) + '\n'
+
+            print line_string
+            print '!!!!===================================================================!!!!'
+
+            warning_log_file.write(line_string)
+            warning_log_file.close()
+
+        # Check the validity of the response string
+        match = re.search('"Status": "OK"', res)
+        match_2 = re.search('"eyebrows": 1010', res)
+        if match and match_2:
+            print "Valid response data"
+        else:
+            print "Invalid response data"
+            warning_log_file = open(FILE_PATH_FOR_WARNING_LOG, 'a')
+
+            line_string = current_time + ' Warning!! Invalid response detected from the face response' + '\n'
+            print '!!!!===================================================================!!!!'
+            print line_string
+            warning_log_file.write(line_string)
+            warning_log_file.close()
+
+        #print res
+
+    except:
         warning_log_file = open(FILE_PATH_FOR_WARNING_LOG, 'a')
 
-        line_string = current_time + ' Warning!! Response time for face recognition is slower than ' \
-                      + str(RESPONSE_TIME_WARINING_THRESHOLD) + 's. Response Time = ' + str(elapsed_time) + '\n'
+        current_time = datetime.datetime.strftime(datetime.datetime.now(), '%Y-%m-%d %H:%M:%S')
+        line_string = current_time + ' Error!! Exception Occurred in face! ' + face_rest_api_address[index] + '\n'
 
         print line_string
         print '!!!!===================================================================!!!!'
 
         warning_log_file.write(line_string)
         warning_log_file.close()
-
-    # Check the validity of the response string
-    match = re.search('"Status": "OK"', res)
-    match_2 = re.search('"eyebrows": 1010', res)
-    if match and match_2:
-        print "Valid response data"
-    else:
-        print "Invalid response data"
-        warning_log_file = open(FILE_PATH_FOR_WARNING_LOG, 'a')
-
-        line_string = current_time + ' Warning!! Invalid response detected from the face response' + '\n'
-        print '!!!!===================================================================!!!!'
-        print line_string
-        warning_log_file.write(line_string)
-        warning_log_file.close()
-
-    #print res
 
     time.sleep(REQUEST_SENDING_INTERVAL)
 

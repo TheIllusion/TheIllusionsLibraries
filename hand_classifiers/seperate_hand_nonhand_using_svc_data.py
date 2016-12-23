5eac719ed1eb6dd81f88a82dc0d56c1899e22ba1
Illusion <gunboy83@gmail.com>
2016-12-23 10:45:53 +0900

    Hand/Nonhand file seperator

--- hand_classifiers/seperate_hand_nonhand_using_svc_data.py
+++ hand_classifiers/seperate_hand_nonhand_using_svc_data.py
@@ -22,9 +22,17 @@ if not os.path.exists(HAND_CANDIDATE_DIR):
 if not os.path.exists(NONHAND_CANDIDATE_DIR):
     os.mkdir(NONHAND_CANDIDATE_DIR)
 
+MAX_INDEX = 50000
+
+index = 0
+
 for jpg_file in original_jpg_files:
     if any(jpg_file in s for s in processed_jpg_files):
         shutil.copy2('ORIGINAL_IMAGE_DIR' + jpg_file, 'HAND_CANDIDATE_DIR' + jpg_file)
     else:
         print 'non-hand: ', jpg_file
         shutil.copy2('ORIGINAL_IMAGE_DIR' + jpg_file, 'NONHAND_CANDIDATE_DIR' + jpg_file)
+
+    index = index + 1
+    if index == MAX_INDEX:
+        break

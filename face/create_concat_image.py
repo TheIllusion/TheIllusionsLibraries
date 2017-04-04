ee308bb4cb393e2f896abe0f139ff321c072f203
Illusion <gunboy83@gmail.com>
2017-04-04 16:39:25 +0900

    Reinforce the exception code

--- face/create_concat_image.py
+++ face/create_concat_image.py
@@ -6,9 +6,10 @@ import os
 import glob
 #import copy
 
-INPUT_DIRECTORY = "/home/nhnent/H1/users/rklee/Data/face_dcgan/canny/"
-ANSWER_DIRECTORY = "/home/nhnent/H1/users/rklee/Data/face_dcgan/extracted_face/"
-OUTPUT_DIRECTORY = "/home/nhnent/H1/users/rklee/Data/face_dcgan/trn_data/"
+INPUT_DIRECTORY = "/Users/Illusion/Documents/Data/palm_data/NEW_DATA_2017/caricature/lab_dlib_extracted_face/extracted_face/"
+ANSWER_DIRECTORY = "/Users/Illusion/Documents/Data/palm_data/NEW_DATA_2017/caricature/lab_dlib_extracted_face/drawings/"
+OUTPUT_DIRECTORY = "/Users/Illusion/Documents/Data/palm_data/NEW_DATA_2017/caricature/lab_dlib_extracted_face/val_data/"
+
 if __name__ == "__main__":
 
     os.chdir(ANSWER_DIRECTORY)
@@ -16,7 +17,7 @@ if __name__ == "__main__":
 
     for jpg_file in jpg_files:
 
-        canny_img = cv2.imread( INPUT_DIRECTORY + 'canny_' + jpg_file, cv2.IMREAD_COLOR)
+        canny_img = cv2.imread( INPUT_DIRECTORY + jpg_file, cv2.IMREAD_COLOR)
         if (type(canny_img) is not np.ndarray):
             print jpg_file + ' load failed!'
             continue
@@ -26,9 +27,12 @@ if __name__ == "__main__":
             print jpg_file + ' load failed!'
             continue
 
+        #try:
         concated_img = np.hstack((canny_img, answer_img))
 
         cv2.imwrite(OUTPUT_DIRECTORY + 'trn_' + jpg_file, concated_img)
+        #except:
+         #   print 'error occurred. skip this image'
 
 
 

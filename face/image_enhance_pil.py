0d7e5a7dc76ceb9799b8bad326e9f8e0389a139c
Illusion <gunboy83@gmail.com>
2017-04-10 16:26:01 +0900

    contrast stretching

---
+++ face/image_enhance_pil.py
@@ -0,0 +1,21 @@
+from PIL import Image
+from PIL import ImageEnhance
+
+import glob
+import os
+
+os.chdir("/Users/Illusion/Desktop/temp/faces_ori")
+
+image_files = glob.glob('*.png')
+
+for filename in image_files:
+
+    image = Image.open(filename)
+
+    enhancer_sharpness = ImageEnhance.Sharpness(image)
+
+    enhancer_contrast = ImageEnhance.Contrast(enhancer_sharpness.enhance(2))
+
+    enhancer_color = ImageEnhance.Color(enhancer_contrast.enhance(2))
+
+    enhancer_color.enhance(0.7).save("result_" + filename)

0c23cd70bb75ed0f9e3a442ac14cb65f0a3bef5a
Illusion <gunboy83@gmail.com>
2017-08-24 15:40:31 +0900

    iou calculator for semantic segmentation (finished)

--- semantic_segmentation/iou_calculator.py
+++ semantic_segmentation/iou_calculator.py
@@ -3,29 +3,27 @@
 import os
 import glob
 import cv2
+import numpy as np
 
 # paths (filenames in GT_IMAGE_DIRECTORY and FEEDFORWARD_IMAGE_DIRECTORY must be the same)
 GT_IMAGE_DIRECTORY = '/Users/Illusion/Temp/seg_test/seg_gt/'
 FEEDFORWARD_IMAGE_DIRECTORY = '/Users/Illusion/Temp/seg_test/seg_modified/'
 
-# list of answer colors in BGR
+# Dictionary of answer colors in BGR. Values must be thresholded to 0 or 1.
+
+answer_classes = {}
 
 # hair (red)
-hair = [0, 0, 255]
+answer_classes['hair'] = [0, 0, 1]
 
 # face and skin (green)
-face_and_skin = [0, 255, 0]
+answer_classes['face_and_skin'] = [0, 1, 0]
 
 # background (blue)
-background = [255, 0, 0]
+answer_classes['background'] = [1, 0, 0]
 
 # clothe (pink)
-# clothe = [255, 0, 255]
-
-answer_colors = []
-answer_colors.append(hair)
-answer_colors.append(face_and_skin)
-answer_colors.append(background)
+answer_classes['clothe'] = [1, 0, 1]
 
 def load_file_names():
     try:
@@ -45,32 +43,157 @@ def load_file_names():
 
     return filenames
 
-def load_pair_images():
-    cv2.
-def calculate_iou():
+def load_pair_images(filename):
+    gt_img = cv2.imread(GT_IMAGE_DIRECTORY + filename, cv2.IMREAD_COLOR)
 
-def calculate_accuracy():
+    result_img = cv2.imread(FEEDFORWARD_IMAGE_DIRECTORY + filename, cv2.IMREAD_COLOR)
+
+    return gt_img, result_img
+
+PIXEL_THRESHOLD = 127
+
+def thresholding_images(gt_img, result_img):
+
+    # thresholding gt_img (first color elements)
+    lower_valued_color_elements = gt_img[..., 0] < PIXEL_THRESHOLD
+    higher_valued_color_elements = np.invert(lower_valued_color_elements)
+    gt_img[lower_valued_color_elements, 0] = 0
+    gt_img[higher_valued_color_elements, 0] = 1
+
+    # thresholding gt_img (second color elements)
+    lower_valued_color_elements = gt_img[..., 1] < PIXEL_THRESHOLD
+    higher_valued_color_elements = np.invert(lower_valued_color_elements)
+    gt_img[lower_valued_color_elements, 1] = 0
+    gt_img[higher_valued_color_elements, 1] = 1
+
+    # thresholding gt_img (third color elements)
+    lower_valued_color_elements = gt_img[..., 2] < PIXEL_THRESHOLD
+    higher_valued_color_elements = np.invert(lower_valued_color_elements)
+    gt_img[lower_valued_color_elements, 2] = 0
+    gt_img[higher_valued_color_elements, 2] = 1
+
+    # thresholding result_img (first color elements)
+    lower_valued_color_elements = result_img[..., 0] < PIXEL_THRESHOLD
+    higher_valued_color_elements = np.invert(lower_valued_color_elements)
+    result_img[lower_valued_color_elements, 0] = 0
+    result_img[higher_valued_color_elements, 0] = 1
+
+    # thresholding result_img (second color elements)
+    lower_valued_color_elements = result_img[..., 1] < PIXEL_THRESHOLD
+    higher_valued_color_elements = np.invert(lower_valued_color_elements)
+    result_img[lower_valued_color_elements, 1] = 0
+    result_img[higher_valued_color_elements, 1] = 1
+
+    # thresholding result_img (third color elements)
+    lower_valued_color_elements = result_img[..., 2] < PIXEL_THRESHOLD
+    higher_valued_color_elements = np.invert(lower_valued_color_elements)
+    result_img[lower_valued_color_elements, 2] = 0
+    result_img[higher_valued_color_elements, 2] = 1
+
+    return gt_img, result_img
+
+def calculate_accuracy(gt_img, result_img):
+    bool_array = np.equal(gt_img, result_img)
+    #pixel_bool_array = all(bool_array_temp[..., 0])
+    unmatched_count = 0
+    total_pixels = gt_img.shape[0] * gt_img.shape[1]
+
+    for i in xrange(gt_img.shape[0]):
+        for j in xrange(gt_img.shape[1]):
+            pixel = bool_array[i][j]
+            if pixel[0] == False or pixel[1] == False or pixel[2] == False:
+                unmatched_count = unmatched_count + 1
+
+    print 'Unmatched pixel count = ', str(unmatched_count)
+    print 'Total pixels = ', str(total_pixels)
+
+    accuracy = float(100) * (total_pixels - unmatched_count) / total_pixels
+
+    return accuracy
+
+def calculate_iou(gt_img, result_img):
+
+    result_area = np.empty([gt_img.shape[0], gt_img.shape[1]], bool)
+    gt_area = np.empty([gt_img.shape[0], gt_img.shape[1]], bool)
+
+    iou_sum = 0
+
+    # iterate through classes (eg. face, hair, background ... )
+    for each_class_name in answer_classes.keys():
+
+        # iterate through pixels
+        for i in xrange(gt_img.shape[0]):
+            for j in xrange(gt_img.shape[1]):
+
+                # detect the class region in ground truth image
+                if all(gt_img[i][j] == answer_classes[each_class_name]):
+                    gt_area[i][j] = True
+                else:
+                    gt_area[i][j] = False
+
+                # detect the class region in feedforward result image
+                if all(result_img[i][j] == answer_classes[each_class_name]):
+                    result_area[i][j] = True
+                else:
+                    result_area[i][j] = False
+
+        print '-------------------------------'
+        print 'class = ', each_class_name
+        print 'class(value) = ', answer_classes[each_class_name]
+
+        # calculate intersection area
+        intersection = np.logical_and(gt_area, result_area)
+        intersection_area = np.sum(intersection)
+        print 'intersection area = ', str(intersection_area)
+
+        # calculate union area
+        union = np.logical_or(gt_area, result_area)
+        union_area = np.sum(union)
+        print 'union area = ', str(union_area)
+
+        if union_area > 0:
+            intersection_over_union = float(100) * intersection_area / union_area
+        else:
+            intersection_over_union = 100
+
+        print 'intersection over union = ', str(intersection_over_union)
+        iou_sum = iou_sum + intersection_over_union
+
+    return iou_sum / len(answer_classes)
 
 if __name__ == "__main__":
 
     filenames = load_file_names()
 
-    for file in filenames:
+    iou_total = 0
+    accuracy_total = 0
+
+    for filename in filenames:
+
         # load the pair of ground truth and feedforward result image
-        gt_img, result_img = load_pair_images()
+        gt_img, result_img = load_pair_images(filename)
 
-        iou = calculate_iou(gt_img, result_img)
+        if ((type(gt_img) is not np.ndarray) or ((type(result_img) is not np.ndarray))):
+            print filename + ' load failed!'
+            break
 
+        # thresholding pixel values to 0 or 1
+        gt_img, result_img = thresholding_images(gt_img, result_img)
+
+        # calculate overall pixel accuracy
         accuracy = calculate_accuracy(gt_img, result_img)
 
-        print '------------------------------'
-        print 'Filename: ', file
-        print 'IOU: ', iou
-        print 'Accuracy: ', accuracy
+        # calculate intersection over union
+        iou = calculate_iou(gt_img, result_img)
 
-    '''
-    for color in answer_colors:
-        # calculate iou
-    '''
+        accuracy_total = accuracy_total + accuracy
+        iou_total = iou_total + iou
 
+        print 'Filename: ', filename
+        print 'Mean IoU: ', iou
+        print 'Pixel Accuracy: ', accuracy
+        print '#################################################'
+
+    print 'Overall Mean IoU = ', float(iou_total) / len(filenames)
+    print 'Overall Pixel Accuracy = ', float(accuracy_total) / len(filenames)
     print 'process finished'

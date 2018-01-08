5cc5fcdaaf218717c7352b52301b618e18525c01
irteam sudo-account <irteamsu@gpuapp-tbt005.svr.toastmaker.net>
2018-01-08 12:46:31 +0900

    u-net (under developing)

--- PyTorch-practice/unet/sub_modules.py
+++ PyTorch-practice/unet/sub_modules.py
@@ -1,1 +1,120 @@
-i
+import torch
+from torch.utils.data import DataLoader
+from torchvision import datasets
+from torch.autograd import Variable
+import torch.nn as nn
+import torch.nn.functional as F
+import torchvision.transforms as transforms
+
+# Layer module from the paper - Table 1.
+class Layer(nn.Module):
+    def __init__(self, kernel_size, in_channels, out_channels):
+
+        super(Layer, self).__init__()
+
+        #self.drop_out = nn.Dropout2d(p=0.2)
+        self.conv = nn.Conv2d(in_channels=in_channels, out_channels=out_channels,
+                              kernel_size=kernel_size, stride=1, padding=1, bias=True)
+        self.batch_norm = nn.BatchNorm2d(out_channels)
+
+    def forward(self, x):
+        #x = self.drop_out(x)
+        x = F.relu(self.conv(x))
+        #x = self.batch_norm(x)
+
+        return x
+
+# max pooling
+class TransitionDown(nn.Module):
+    def __init__(self, in_channels):
+        super(TransitionDown, self).__init__()
+
+        '''
+        self.drop_out = nn.Dropout2d(p=0.2)
+        self.conv = nn.Conv2d(in_channels=in_channels, out_channels=in_channels,
+                              kernel_size=1, stride=1, bias=True)
+        out_channels = in_channels
+        self.batch_norm = nn.BatchNorm2d(out_channels)
+        '''
+
+    def forward(self, x):
+        x = F.max_pool2d(input=x, kernel_size=2)
+        #x = self.drop_out(x)
+        #x = F.relu(self.conv(x))
+        #x = self.batch_norm(x)
+
+        return x
+
+# Transition Up (TU) module 
+class TransitionUp(nn.Module):
+    def __init__(self, in_channels):
+        super(TransitionUp, self).__init__()
+
+        '''
+        # nn.ConvTranspose2d
+        Args
+        in_channels (int): Number of channels in the input image
+        out_channels (int): Number of channels produced by the convolution
+        kernel_size (int or tuple): Size of the convolving kernel
+        stride (int or tuple, optional): Stride of the convolution
+        padding (int or tuple, optional): Zero-padding added to both sides of the input
+        output_padding (int or tuple, optional): Zero-padding added to one side of the output
+        groups (int, optional): Number of blocked connections from input channels to output channels
+        bias (bool, optional): If True, adds a learnable bias to the output
+        dilation (int or tuple, optional): Spacing between kernel elements
+        '''
+
+        self.transpoed_conv = nn.ConvTranspose2d(in_channels=in_channels, out_channels=in_channels/2,
+                                                 kernel_size=2, stride=2, padding=0, output_padding=0, bias=True)
+
+    def forward(self, x):
+        x = self.transpoed_conv(x)
+        return x
+
+# Dense Block
+'''
+class DenseBlock(nn.Module):
+    def __init__(self, layers, in_channels, k_feature_maps, is_gpu_mode):
+        super(DenseBlock, self).__init__()
+
+        self.num_layers = layers
+        self.layers_list = []
+
+        # add layers to the list
+        for i in xrange(layers):
+            self.layers_list.append(Layer(kernel_size=3,
+                                          in_channels=in_channels + i*k_feature_maps,
+                                          out_channels=k_feature_maps))
+
+        # gpu
+        if is_gpu_mode:
+            for model in self.layers_list:
+                model.cuda()
+
+    def forward(self, x):
+
+        # feedforward x to the first layer and add the result to the list
+        x_first_out = self.layers_list[0].forward(x)
+
+        # initialize the list
+        forwarded_output_list = []
+
+        forwarded_output_list.append(x_first_out)
+        prev_x = x
+
+        # feedforward process from the second to the last layer
+        for i in range(1, self.num_layers):
+            # concatenate filters
+            concatenated_filters = torch.cat((forwarded_output_list[i-1], prev_x), 1)
+            # forward
+            x_next_out = self.layers_list[i].forward(concatenated_filters)
+            # add to the list
+            forwarded_output_list.append(x_next_out)
+            # prepare the temporary variable for the next loop
+            prev_x = concatenated_filters
+
+        # prepare the output (this will have (k_feature_maps * layers) feature maps)
+        output_x = torch.cat(forwarded_output_list, 1)
+
+        return output_x
+'''
--- PyTorch-practice/unet/unet.py
+++ PyTorch-practice/unet/unet.py
@@ -1,1 +1,370 @@
-i
+import torch
+from torch.utils.data import DataLoader
+from torchvision import datasets
+from torch.autograd import Variable
+import torch.nn as nn
+import torch.nn.functional as F
+#import torchvision.transforms as transforms
+from sub_modules import Layer, TransitionUp, TransitionDown, DenseBlock
+import time, cv2
+import numpy as np
+
+# gpu mode
+is_gpu_mode = True 
+
+# feedforward mode
+is_feedforward_mode = False
+
+if not is_feedforward_mode:
+    import data_loader
+    
+# batch size
+BATCH_SIZE = 2
+TOTAL_ITERATION = 1000000
+
+# model saving (iterations)
+MODEL_SAVING_FREQUENCY = 1000
+# i7-2600k
+#MODEL_SAVING_DIRECTORY = '/home/illusion/PycharmProjects/TheIllusionsLibraries/PyTorch-practice/tiramisu-fcdensenet103/models/'
+# macbook pro
+#MODEL_SAVING_DIRECTORY = '/Users/Illusion/PycharmProjects/TheIllusionsLibraries/PyTorch-practice/tiramisu-fcdensenet103/models/'
+# tbt005
+MODEL_SAVING_DIRECTORY = '/home1/irteamsu/rklee/TheIllusionsLibraries/PyTorch-practice/unet/models/'
+
+# macbook pro
+#cifar10_data_dir = '/Users/Illusion/PycharmProjects/TheIllusionsLibraries/PyTorch-practice/fashion-mnist/data/'
+
+# The output of torchvision datasets are PILImage images of range [0, 1].
+# We transform them to Tensors of normalized range [-1, 1]
+'''
+transform = transforms.Compose(
+    [transforms.ToTensor(),
+     transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))])
+'''
+
+# cifar10 dataset
+'''
+CIFA-10 dataset (Docs: https://www.cs.toronto.edu/~kriz/cifar.html)
+The CIFAR-10 dataset consists of 60000 32x32 colour images in 10 classes, with 6000 images per class. There are 50000 training images and 10000 test images. 
+'''
+
+'''
+train_set = datasets.CIFAR10(cifar10_data_dir, train=True,
+                             transform=transform, target_transform=None,
+                             download=True)
+
+train_loader = DataLoader(train_set, batch_size=BATCH_SIZE, shuffle=True, num_workers=2)
+
+test_set = datasets.CIFAR10(cifar10_data_dir, train=False,
+                            transform=transform, target_transform=None,
+                            download=True)
+
+test_loader = DataLoader(test_set, batch_size=BATCH_SIZE, shuffle=False, num_workers=2)
+'''
+
+class Unet(nn.Module):
+    def __init__(self):
+        """
+        In the constructor we instantiate our custom modules and assign them as
+        member variables.
+        """
+        super(Unet, self).__init__()
+
+        # unet architecture
+        
+        # first conv
+        self.first_conv_layer = Layer(kernel_size=3, in_channels=3, out_channels=64)
+        
+        # second conv
+        self.second_conv_layer = Layer(kernel_size=3, in_channels=64, out_channels=64)
+        
+        # max pool
+        self.first_transition_down = TransitionDown(64)
+        
+        # third conv
+        self.third_conv_layer = Layer(kernel_size=3, in_channels=64, out_channels=128)
+        
+        # fourth conv
+        self.fourth_conv_layer = Layer(kernel_size=3, in_channels=128, out_channels=128)
+        
+        # max pool
+        self.second_transition_down = TransitionDown(128)
+        
+        # fifth conv
+        self.fifth_conv_layer = Layer(kernel_size=3, in_channels=128, out_channels=256)
+        
+        # sixth conv
+        self.sixth_conv_layer = Layer(kernel_size=3, in_channels=256, out_channels=256)
+        
+        # max pool
+        self.third_transition_down = TransitionDown(256)
+        
+        # seventh conv
+        self.seventh_conv_layer = Layer(kernel_size=3, in_channels=256, out_channels=512)
+        
+        # eighth conv
+        self.eighth_conv_layer = Layer(kernel_size=3, in_channels=512, out_channels=512)
+        
+        # max pool
+        self.fourth_transition_down = TransitionDown(512)
+        
+        # ninth conv
+        self.ninth_conv_layer = Layer(kernel_size=3, in_channels=512, out_channels=1024)
+        
+        # tenth conv
+        self.tenth_conv_layer = Layer(kernel_size=3, in_channels=1024, out_channels=1024)
+        
+        # first Transition Up
+        self.later_first_transition_up = TransitionUp(1024)
+        
+        # followed by 2 conv layers
+        self.later_first_conv_layer = Layer(kernel_size=3, in_channels=1024, out_channels=512)
+        self.later_second_conv_layer = Layer(kernel_size=3, in_channels=512, out_channels=512)
+        
+        # second Transition Up
+        self.later_second_transition_up = TransitionUp(512)
+        
+        # followed by 2 conv layers 
+        self.later_third_conv_layer = Layer(kernel_size=3, in_channels=512, out_channels=256)
+        self.later_fourth_conv_layer = Layer(kernel_size=3, in_channels=256, out_channels=256)
+        
+        # third Transition Up
+        self.later_third_transition_up = TransitionUp(256)
+        
+        # followed by 2 conv layers 
+        self.later_fifth_conv_layer = Layer(kernel_size=3, in_channels=256, out_channels=128)
+        self.later_sixth_conv_layer = Layer(kernel_size=3, in_channels=128, out_channels=128)
+        
+        # fourth Transition Up
+        self.later_fourth_transition_up = TransitionUp(128)
+        
+        # followed by 2 conv layers 
+        self.later_seventh_conv_layer = Layer(kernel_size=3, in_channels=128, out_channels=64)
+        self.later_eight_conv_layer = Layer(kernel_size=3, in_channels=64, out_channels=64)
+        
+        # last conv layer (num. of output channels should be modified according to the number of classes)
+        self.later_last_conv_layer = Layer(kernel_size=3, in_channels=64, out_channels=3)
+
+    def forward(self, x):
+        """
+        In the forward function we accept a Variable of input data and we must return
+        a Variable of output data. We can use Modules defined in the constructor as
+        well as arbitrary operators on Variables.
+        """
+
+        # unet
+        ######################################################################
+        # define the forward connections
+        ######################################################################
+        x = self.first_conv_layer(x)
+        x = self.second_conv_layer(x)
+        
+        # first TD
+        x = self.first_transition_down(x)
+        
+        x = self.third_conv_layer(x)
+        x = self.fourth_conv_layer(x)
+        
+        # second TD
+        x = self.second_transition_down(x)
+        
+        x = self.fifth_conv_layer(x)
+        x = self.sixth_conv_layer(x)
+        
+        # third TD
+        x = self.third_transition_down(x)
+        
+        x = self.seventh_conv_layer(x)
+        x = self.eight_conv_layer(x)
+        
+        # fourth TD
+        x = self.fourth_transition_down(x)
+        
+        ######################################################################
+        # define the middle connections
+        ######################################################################
+        x = self.nigth_conv_layer(x)
+        x = self.tenth_conv_layer(x)
+        
+        ######################################################################
+        # define the backward connections
+        ######################################################################
+        # first TU
+        # second TU
+        # third TU
+        # fourth TU
+        
+        # code borrowed from Tiramisu
+        ######################################################################
+        # define the forward connections
+        ######################################################################
+        x_first_conv_out = self.first_conv_layer(x)
+
+        x_first_dense_out = self.first_dense_block(x_first_conv_out)
+        # concatenate filters
+        x_first_dense_out_concat = torch.cat((x_first_conv_out, x_first_dense_out), 1)
+        x_first_td_out = self.first_transition_down(x_first_dense_out_concat)
+
+        x_second_dense_out = self.second_dense_block(x_first_td_out)
+        # concatenate filters
+        x_second_dense_out_concat = torch.cat((x_first_td_out, x_second_dense_out), 1)
+        x_second_td_out = self.second_transition_down(x_second_dense_out_concat)
+
+        x_third_dense_out = self.third_dense_block(x_second_td_out)
+        # concatenate filters
+        x_third_dense_out_concat = torch.cat((x_second_td_out, x_third_dense_out), 1)
+        x_third_td_out = self.third_transition_down(x_third_dense_out_concat)
+
+        x_fourth_dense_out = self.fourth_dense_block(x_third_td_out)
+        # concatenate filters
+        x_fourth_dense_out_concat = torch.cat((x_third_td_out, x_fourth_dense_out), 1)
+        x_fourth_td_out = self.fourth_transition_down(x_fourth_dense_out_concat)
+
+        x_fifth_dense_out = self.fifth_dense_block(x_fourth_td_out)
+        # concatenate filters
+        x_fifth_dense_out_concat = torch.cat((x_fourth_td_out, x_fifth_dense_out), 1)
+        x_fifth_td_out = self.fifth_transition_down(x_fifth_dense_out_concat)
+
+        x_middle_dense_out = self.middle_dense_block(x_fifth_td_out)
+
+        ######################################################################
+        # define the backward connections
+        ######################################################################
+        x_later_first_tu_out = self.later_first_transition_up(x_middle_dense_out)
+        # concatenate filters (Skip-Connection)
+        x_later_first_tu_out_concat = torch.cat((x_later_first_tu_out, x_fifth_dense_out_concat), 1)
+        x_later_first_dense_out = self.later_first_dense_block(x_later_first_tu_out_concat)
+
+        x_later_second_tu_out = self.later_second_transition_up(x_later_first_dense_out)
+        # concatenate filters (Skip-Connection)
+        x_later_second_tu_out_concat = torch.cat((x_later_second_tu_out, x_fourth_dense_out_concat), 1)
+        x_later_second_dense_out = self.later_second_dense_block(x_later_second_tu_out_concat)
+
+        x_later_third_tu_out = self.later_third_transition_up(x_later_second_dense_out)
+        # concatenate filters (Skip-Connection)
+        x_later_third_tu_out_concat = torch.cat((x_later_third_tu_out, x_third_dense_out_concat), 1)
+        x_later_third_dense_out = self.later_third_dense_block(x_later_third_tu_out_concat)
+
+        x_later_fourth_tu_out = self.later_fourth_transition_up(x_later_third_dense_out)
+        # concatenate filters (Skip-Connection)
+        x_later_fourth_tu_out_concat = torch.cat((x_later_fourth_tu_out, x_second_dense_out_concat), 1)
+        x_later_fourth_dense_out = self.later_fourth_dense_block(x_later_fourth_tu_out_concat)
+
+        x_later_fifth_tu_out = self.later_fifth_transition_up(x_later_fourth_dense_out)
+        # concatenate filters (Skip-Connection)
+        x_later_fifth_tu_out_concat = torch.cat((x_later_fifth_tu_out, x_first_dense_out_concat), 1)
+        x_later_fifth_dense_out = self.later_fifth_dense_block(x_later_fifth_tu_out_concat)
+
+        x_last_conv_out = self.last_conv_layer(x_later_fifth_dense_out)
+
+        sigmoid_out = nn.functional.sigmoid(x_last_conv_out)
+        #softmax_out = custom_softmax_for_segmentation(x_last_conv_out, 3)
+
+        #output = sigmoid_out * 255
+        #output = softmax_out
+
+        return sigmoid_out
+
+if __name__ == "__main__":
+    print 'main'
+
+    unet_model = Unet()
+
+    if is_gpu_mode:
+        unet_model.cuda()
+
+    # Use the optim package to define an Optimizer that will update the weights of
+    # the model for us. Here we will use Adam; the optim package contains many other
+    # optimization algoriths. The first argument to the Adam constructor tells the
+    # optimizer which Variables it should update.
+    learning_rate = 1e-4
+
+    # Construct our loss function and an Optimizer. The call to model.parameters()
+    # in the SGD constructor will contain the learnable parameters of the two
+    # nn.Linear modules which are members of the model.
+    #criterion = torch.nn.MSELoss(size_average=False)
+    criterion = torch.nn.BCELoss()
+
+    # optimizer = torch.optim.SGD(model.parameters(), lr=1e-4)
+    optimizer = torch.optim.Adam(tiramisu_model.parameters(), lr=learning_rate)
+
+    # read imgs
+    image_buff_read_index = 0
+
+    # opencv style
+    '''
+    input_img = np.empty(shape=(BATCH_SIZE, 512, 512, 3))
+    answer_img = np.empty(shape=(BATCH_SIZE, 512, 512, 3))
+    '''
+
+    # pytorch style
+    input_img = np.empty(shape=(BATCH_SIZE, 3, data_loader.INPUT_IMAGE_WIDTH, data_loader.INPUT_IMAGE_HEIGHT))
+    answer_img = np.empty(shape=(BATCH_SIZE, 3, data_loader.INPUT_IMAGE_WIDTH, data_loader.INPUT_IMAGE_HEIGHT))
+
+    for i in range(TOTAL_ITERATION):
+
+        # print 'iter: ', str(iter)
+        exit_notification = False
+
+        for j in range(BATCH_SIZE):
+
+            data_loader.is_main_alive = True
+
+            while data_loader.buff_status[image_buff_read_index] == 'empty':
+                if exit_notification == True:
+                    break
+
+                time.sleep(1)
+                if data_loader.buff_status[image_buff_read_index] == 'filled':
+                    break
+
+            if exit_notification == True:
+                break
+
+            np.copyto(input_img[j], data_loader.input_buff[image_buff_read_index])
+            np.copyto(answer_img[j], data_loader.answer_buff[image_buff_read_index])
+
+            data_loader.buff_status[image_buff_read_index] = 'empty'
+
+            image_buff_read_index = image_buff_read_index + 1
+            if image_buff_read_index >= data_loader.image_buffer_size:
+                image_buff_read_index = 0
+
+            if is_gpu_mode:
+                inputs, answers = Variable(torch.from_numpy(input_img).float().cuda()), \
+                                  Variable(torch.from_numpy(answer_img).float().cuda())
+            else:
+                inputs, answers = Variable(torch.from_numpy(input_img).float()), Variable(torch.from_numpy(answer_img).float())
+
+        answers = answers / 255.
+        #answers = answers.long()
+
+        # Before the backward pass, use the optimizer object to zero all of the
+        # gradients for the variables it will update (which are the learnable weights
+        # of the model)
+        optimizer.zero_grad()
+
+        outputs = unet_model(inputs)
+
+        # change the dimension
+        outputs_view = outputs.view(BATCH_SIZE * data_loader.INPUT_IMAGE_WIDTH * data_loader.INPUT_IMAGE_HEIGHT * 3)
+        answers_view = answers.view(BATCH_SIZE * data_loader.INPUT_IMAGE_WIDTH * data_loader.INPUT_IMAGE_HEIGHT * 3)
+
+        #loss = criterion(outputs, answers)
+        loss = criterion(outputs_view, answers_view)
+
+        # Backward pass: compute gradient of the loss with respect to model parameters
+        loss.backward()
+
+        # Calling the step function on an Optimizer makes an update to its parameters
+        optimizer.step()
+
+        if i % 10 == 0:
+            print '-----------------------------------'
+            print 'iterations = ', str(i)
+            print 'loss = ', str(loss)
+
+        # save the model
+        if i % MODEL_SAVING_FREQUENCY == 0:
+            torch.save(tiramisu_model.state_dict(),
+                       MODEL_SAVING_DIRECTORY + 'unet_iter_' +str(i) + '.pt')

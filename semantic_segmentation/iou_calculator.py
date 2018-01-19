4674894ee1e2ca976c9db79c81927f120114908c
Illusion <gunboy83@gmail.com>
2018-01-19 09:48:11 +0900

    ppm_to_jpg util is added

--- semantic_segmentation/iou_calculator.py
+++ semantic_segmentation/iou_calculator.py
@@ -5,7 +5,7 @@ import glob
 import cv2
 import numpy as np
 
-PIXEL_THRESHOLD = 110
+PIXEL_THRESHOLD = 130
 #PIXEL_THRESHOLD = 160
 
 # paths (filenames in GT_IMAGE_DIRECTORY and FEEDFORWARD_IMAGE_DIRECTORY must be the same)
@@ -20,7 +20,7 @@ GT_IMAGE_DIRECTORY = '/Users/Illusion/Documents/Data/hair_semantic_segmentation/
 #FEEDFORWARD_IMAGE_DIRECTORY = '/Users/Illusion/Documents/Data/hair_semantic_segmentation/feedforward_result/forward_result_until_0823_and_lfw_aug/'
 #FEEDFORWARD_IMAGE_DIRECTORY = '/Users/Illusion/Documents/Data/hair_semantic_segmentation/feedforward_result/forward_result_until_0823_aug/'
 #FEEDFORWARD_IMAGE_DIRECTORY = '/Users/Illusion/Downloads/forward_result_unet/'
-FEEDFORWARD_IMAGE_DIRECTORY = '/Users/Illusion/Documents/Data/hair_semantic_segmentation/feedforward_result/custom_unet_and_tiramisu/tiramisu_lr_0_0003/'
+FEEDFORWARD_IMAGE_DIRECTORY = '/Users/Illusion/Downloads/tiramisu_out_lr_0_0001/'
 
 # pink to blue by photoshop
 #FEEDFORWARD_IMAGE_DIRECTORY = '/Users/Illusion/Documents/Data/hair_semantic_segmentation/feedforward_result/forward_result_until_0823_background_and_geometry_aug_pink_to_blue/'

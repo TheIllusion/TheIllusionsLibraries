8a095d994b7da5dae99166eb056213c0b54a7a18
Illusion <gunboy83@gmail.com>
2018-02-05 11:35:19 +0900

    encoding tool with psnr - seems to work

--- image_preprocessing_utils/python-compare-two-images/jpg_encoding_with_psnr.py
+++ image_preprocessing_utils/python-compare-two-images/jpg_encoding_with_psnr.py
@@ -1,4 +1,3 @@
-#from skimage.measure import structural_similarity as ssim
 from skimage.measure import compare_ssim
 import numpy as np
 import math
@@ -6,19 +5,7 @@ import cv2
 
 MAX_PIXEL_VALUE = 255.0
 
-'''
-def mse(imageA, imageB):
-    # the 'Mean Squared Error' between the two images is the
-    # sum of the squared difference between the two images;
-    # NOTE: the two images must have the same dimension
-    err = np.sum((imageA.astype("float") - imageB.astype("float")) ** 2)
-    err /= float(imageA.shape[0] * imageA.shape[1])
-
-    # return the MSE, the lower the error, the more "similar"
-    # the two images are
-    return err
-'''
-
+# PSNR CALCULATION FUNC
 def get_psnr(img1, img2):
     mse = np.mean( (img1 - img2) ** 2 )
     if mse == 0:
@@ -27,59 +14,84 @@ def get_psnr(img1, img2):
     psnr = 20 * math.log10(MAX_PIXEL_VALUE / math.sqrt(mse))
     return psnr
 
+# SSIM CALCULATION FUNC
 def get_ssim(img1, img2):
     ssim = compare_ssim(img1, img2, multichannel=True)
     return ssim
 
-def compare_images(imageA, imageB):
-    # compute the mean squared error and structural similarity
-    # index for the images
-    p = get_psnr(imageA, imageB)
-    s = get_ssim(imageA, imageB)
+# SINGLE ENCODING TRIAL
+def try_encode_a_jpg(input_img, jpeg_encoding_quality, output_filename):
 
-    print 'psnr =', p
-    print 'ssime =', s
+    cv2.imwrite(output_filename, input_img, [int(cv2.IMWRITE_JPEG_QUALITY), jpeg_encoding_quality])
+    decoded_img = cv2.imread(output_filename, cv2.IMREAD_UNCHANGED)
 
-def encode_jpg_image_at_target_psnr(input_img, target_psnr):
+    psnr_val = get_psnr(input_img, decoded_img)
+    ssim_val = get_ssim(input_img, decoded_img)
+    return psnr_val, ssim_val
 
-    # initial encoding quality
-    jpeg_encoding_quality = 100
-    cv2.imwrite('output_' + str(target_psnr) + '.jpg', input_img, [int(cv2.IMWRITE_JPEG_QUALITY), jpeg_encoding_quality])
-    decoded_img = cv2.imread('output_' + str(target_psnr) + '.jpg', cv2.IMREAD_UNCHANGED)
-    max_psnr_val = get_psnr(input_img, decoded_img)
-    print 'max_psnr_val =', max_psnr_val
+def encode_jpg_image_at_target_psnr(input_img, target_psnr_val, output_jpg_filename):
 
-    jpeg_encoding_quality = 0
-    cv2.imwrite('output_' + str(target_psnr) + '.jpg', input_img, [int(cv2.IMWRITE_JPEG_QUALITY), jpeg_encoding_quality])
-    decoded_img = cv2.imread('output_' + str(target_psnr) + '.jpg', cv2.IMREAD_UNCHANGED)
-    min_psnr_val = get_psnr(input_img, decoded_img)
-    print 'min_psnr_val =', min_psnr_val
+    # dict: (quality_factor, psnr_diff)
+    psnr_diff_dict = {}
 
+    # initial max/min jpeg encoding quality (between 0,100)
+    max_quality_trial_factor = 100
+    min_quality_trial_factor = 0
 
+    # initial encoding quality (the best quality)
+    max_psnr_val, _ = try_encode_a_jpg(input_img, max_quality_trial_factor, output_jpg_filename)
+    print 'possible max_psnr_val =', max_psnr_val
 
+    # initial encoding quality (the lowest quality)
+    min_psnr_val, _ = try_encode_a_jpg(input_img, min_quality_trial_factor, output_jpg_filename)
+    print 'possible min_psnr_val =', min_psnr_val
 
-    print psnr_val
+    # error handling if the target psnr is out of the possible range
+    if target_psnr_val < min_psnr_val or target_psnr_val > max_psnr_val:
+        print 'The target psnr value cannot be achieved.'
+        print 'The possible psnr range for this image is between', min_psnr_val, 'and', max_psnr_val
+        return False
 
-# load the images -- the original, the original + contrast,
-# and the original + photoshop
-original = cv2.imread("images/jp_gates_original.png")
-original_grayscale = cv2.imread("images/jp_gates_original.png", cv2.IMREAD_GRAYSCALE)
-contrast = cv2.imread("images/jp_gates_contrast.png")
-shopped = cv2.imread("images/jp_gates_photoshopped.png")
+    # find the best matching quality factor
+    while True:
+        print '---------------------------------------------------------------------------'
+        middle_quality_trial_factor = (max_quality_trial_factor + min_quality_trial_factor) / 2
 
-encode_jpg_image_at_psnr(original, 50)
+        if middle_quality_trial_factor in psnr_diff_dict:
+            break
 
-'''
-print 'original vs original'
-compare_images(original, original)
+        print 'try encoding with the jpg encoding quality factor', middle_quality_trial_factor
+        current_psnr_val, current_ssim_val = try_encode_a_jpg(input_img, middle_quality_trial_factor, output_jpg_filename)
+        print 'current psnr value =', current_psnr_val
+        print 'current_ssim_val =', current_ssim_val
 
-print 'original vs original(grayscale)'
-compare_images(original_grayscale, original_grayscale)
+        # record the data
+        psnr_diff_dict[middle_quality_trial_factor] = abs(float(target_psnr_val) - current_psnr_val)
 
-print 'original vs contrast'
-compare_images(original, contrast)
+        # update the next search range
+        if current_psnr_val > target_psnr_val:
+            max_quality_trial_factor = middle_quality_trial_factor
+        else:
+            min_quality_trial_factor = middle_quality_trial_factor
 
-print 'original vs photoshopped'
-compare_images(original, shopped)
-'''
+    # find the minimum absolute difference between target psnr and actual psnr
+    best_matching_quality_factor = min(psnr_diff_dict, key=psnr_diff_dict.get)
+    print 'best matching jpg encoding quality factor =', best_matching_quality_factor
 
+    # encode the actual jpg image
+    final_psnr_val, final_ssim_val = try_encode_a_jpg(input_img, best_matching_quality_factor, output_jpg_filename)
+    print 'result psnr value =', final_psnr_val
+    print 'result ssim value =', final_ssim_val
+
+    return True
+
+if __name__ == '__main__':
+    # load the images -- the original, the original + contrast,
+    # and the original + photoshop
+    original = cv2.imread("images/jp_gates_original.png")
+    original_grayscale = cv2.imread("images/jp_gates_original.png", cv2.IMREAD_GRAYSCALE)
+    contrast = cv2.imread("images/jp_gates_contrast.png")
+    shopped = cv2.imread("images/jp_gates_photoshopped.png")
+
+    output_jpg_filename = 'output_encode_trial.jpg'
+    encode_jpg_image_at_target_psnr(input_img=original, target_psnr_val=34, output_jpg_filename=output_jpg_filename)

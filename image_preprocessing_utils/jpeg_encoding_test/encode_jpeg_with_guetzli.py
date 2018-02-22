9ae1d2067ac2c138fa990376921996822a7afbf4
TheIllusion <gunboy83@gmail.com>
2018-02-23 05:46:25 +0900

    add encode_jpeg_with_guetzli

---
+++ image_preprocessing_utils/jpeg_encoding_test/encode_jpeg_with_guetzli.py
@@ -0,0 +1,39 @@
+import os, glob, time
+
+QUALITY_FACTOR = 85
+
+INPUT_IMAGE_DIRECTORY = '/Users/Illusion/Documents/data/shopping_mall_images/mini_testset/'
+OUTPUT_IMAGE_DIRECTORY = '/Users/Illusion/Documents/data/shopping_mall_images/mini_testset_guetzli_q' + str(QUALITY_FACTOR) + '/'
+LOG_FILE = OUTPUT_IMAGE_DIRECTORY + 'q' + str(QUALITY_FACTOR) + '_process_log.txt'
+
+if not os.path.exists(OUTPUT_IMAGE_DIRECTORY):
+    os.mkdir(OUTPUT_IMAGE_DIRECTORY)
+
+if not os.path.exists(INPUT_IMAGE_DIRECTORY):
+    exit(0)
+else:
+    os.chdir(INPUT_IMAGE_DIRECTORY)
+
+log_file = open(LOG_FILE, "w")
+log_file.write("filename process_time\n")
+
+jpg_files = glob.glob('*.jpg')
+
+for jpg_file in jpg_files[0:1]:
+    command_str = "guetzli --verbose --quality " + str(QUALITY_FACTOR) + " --nomemlimit " + \
+                   jpg_file + " " + \
+                   OUTPUT_IMAGE_DIRECTORY + "output_q" + str(QUALITY_FACTOR) + '_' + jpg_file
+
+    print command_str
+
+    start_time = time.time()
+
+    os.system(command_str)
+
+    elapsed_time = time.time() - start_time
+
+    log_file.write(jpg_file + " " + str(elapsed_time))
+
+    print jpg_file + ' finished. elapsed_time =', str(elapsed_time) + '\n'
+
+log_file.close()

pytorch practice - srgan with vgg loss (correct normalization technique applied)

PyTorch-practice/sr_gan_tiramisu/generator_modified_tiramisu.py
@@ -10,21 +10,18 @@ class Layer(nn.Module):
 
         self.drop_out = nn.Dropout2d(p=0.2)
         self.conv = nn.Conv2d(in_channels=in_channels, out_channels=out_channels,
-                              kernel_size=kernel_size, stride=1, padding=1, bias=True)
+                              kernel_size=kernel_size, stride=1, padding=1, bias=False)
 
         # weight initialization
-        #torch.nn.init.xavier_uniform(self.conv.weight)
-        torch.nn.init.kaiming_uniform(self.conv.weight)
+        torch.nn.init.xavier_uniform(self.conv.weight)
+        #torch.nn.init.kaiming_uniform(self.conv.weight)
         
         self.batch_norm = nn.BatchNorm2d(out_channels)
 
     def forward(self, x):
-        #x = self.drop_out(x)
+        x = self.drop_out(x)
         x = F.relu(self.conv(x))
         x = self.batch_norm(x)
-        
-        # substitution for the batch_norm (by rklee)
-        #x = F.relu(x)
 
         return x
 
@@ -35,18 +32,18 @@ class TransitionDown(nn.Module):
 
         self.drop_out = nn.Dropout2d(p=0.2)
         self.conv = nn.Conv2d(in_channels=in_channels, out_channels=in_channels,
-                              kernel_size=1, stride=1, bias=True)
+                              kernel_size=1, stride=1, bias=False)
 
         # weight initialization
-        #torch.nn.init.xavier_uniform(self.conv.weight)
-        torch.nn.init.kaiming_uniform(self.conv.weight)
+        torch.nn.init.xavier_uniform(self.conv.weight)
+        #torch.nn.init.kaiming_uniform(self.conv.weight)
 
         out_channels = in_channels
         self.batch_norm = nn.BatchNorm2d(out_channels)
 
     def forward(self, x):
         x = F.max_pool2d(input=x, kernel_size=2)
-        #x = self.drop_out(x)
+        x = self.drop_out(x)
         x = F.relu(self.conv(x))
         x = self.batch_norm(x)
 
@@ -72,11 +69,11 @@ class TransitionUp(nn.Module):
         '''
 
         self.transpoed_conv = nn.ConvTranspose2d(in_channels=in_channels, out_channels=in_channels,
-                                                 kernel_size=2, stride=2, padding=0, output_padding=0, bias=True)
+                                                 kernel_size=2, stride=2, padding=0, output_padding=0, bias=False)
 
         # weight initialization
-        #torch.nn.init.xavier_uniform(self.transpoed_conv.weight)
-        torch.nn.init.kaiming_uniform(self.transpoed_conv.weight)
+        torch.nn.init.xavier_uniform(self.transpoed_conv.weight)
+        #torch.nn.init.kaiming_uniform(self.transpoed_conv.weight)
         
     def forward(self, x):
         x = self.transpoed_conv(x)

PyTorch-practice/sr_gan_tiramisu/prev_srgan_with_vgg_feature_loss.py
@@ -147,6 +147,51 @@ def load_image(image_path, transform=None, max_size=None, shape=None):
     return image.type(dtype)
 '''
 
+'''
+# vgg19 structure
+self.vgg._modules.items() = 
+[
+('0', Conv2d (3, 64, kernel_size=(3, 3), stride=(1, 1), padding=(1, 1))), 
+('1', ReLU(inplace)), 
+('2', Conv2d (64, 64, kernel_size=(3, 3), stride=(1, 1), padding=(1, 1))), 
+('3', ReLU(inplace)), 
+('4', MaxPool2d(kernel_size=(2, 2), stride=(2, 2), dilation=(1, 1))), 
+('5', Conv2d (64, 128, kernel_size=(3, 3), stride=(1, 1), padding=(1, 1))), 
+('6', ReLU(inplace)), 
+('7', Conv2d (128, 128, kernel_size=(3, 3), stride=(1, 1), padding=(1, 1))), 
+('8', ReLU(inplace)), 
+('9', MaxPool2d(kernel_size=(2, 2), stride=(2, 2), dilation=(1, 1))), 
+('10', Conv2d (128, 256, kernel_size=(3, 3), stride=(1, 1), padding=(1, 1))), 
+('11', ReLU(inplace)), 
+('12', Conv2d (256, 256, kernel_size=(3, 3), stride=(1, 1), padding=(1, 1))), 
+('13', ReLU(inplace)), 
+('14', Conv2d (256, 256, kernel_size=(3, 3), stride=(1, 1), padding=(1, 1))), 
+('15', ReLU(inplace)), 
+('16', Conv2d (256, 256, kernel_size=(3, 3), stride=(1, 1), padding=(1, 1))), 
+('17', ReLU(inplace)), 
+('18', MaxPool2d(kernel_size=(2, 2), stride=(2, 2), dilation=(1, 1))), 
+('19', Conv2d (256, 512, kernel_size=(3, 3), stride=(1, 1), padding=(1, 1))), 
+('20', ReLU(inplace)), 
+('21', Conv2d (512, 512, kernel_size=(3, 3), stride=(1, 1), padding=(1, 1))),
+('22', ReLU(inplace)), 
+('23', Conv2d (512, 512, kernel_size=(3, 3), stride=(1, 1), padding=(1, 1))), ('24', ReLU(inplace)), 
+('25', Conv2d (512, 512, kernel_size=(3, 3), stride=(1, 1), padding=(1, 1))), 
+('26', ReLU(inplace)), 
+('27', MaxPool2d(kernel_size=(2, 2), stride=(2, 2), dilation=(1, 1))), 
+('28', Conv2d (512, 512, kernel_size=(3, 3), stride=(1, 1), padding=(1, 1))), 
+('29', ReLU(inplace)), 
+('30', Conv2d (512, 512, kernel_size=(3, 3), stride=(1, 1), padding=(1, 1))), 
+('31', ReLU(inplace)), 
+('32', Conv2d (512, 512, kernel_size=(3, 3), stride=(1, 1), padding=(1, 1))), 
+('33', ReLU(inplace)), 
+('34', Conv2d (512, 512, kernel_size=(3, 3), stride=(1, 1), padding=(1, 1))), 
+('35', ReLU(inplace)), 
+('36', MaxPool2d(kernel_size=(2, 2), stride=(2, 2), dilation=(1, 1)))
+]
+
+conv_layers = [0,2,5,7,10,12,14,16,19,21,23,25,28,30,32,34]
+'''
+
 # Pretrained VGGNet 
 class VGGNet(nn.Module):
     def __init__(self):
@@ -154,7 +199,7 @@ class VGGNet(nn.Module):
         
         """Select conv1_1 ~ conv5_1 activation maps."""
         #self.select = ['0', '5', '10', '19', '28'] 
-        self.select = ['0','1','2','3','4','5','6','7','8','9','10','12','14','16','19','22','25','28'] 
+        self.select = ['0','2','5','7','10','12','14','16','19','21','23','25','28','30','32','34'] 
         self.vgg = models.vgg19(pretrained=True).features
         
     def forward(self, x):
d231e9a97b7486add5ff81b9e15c0f058354fa4f
Illusion <gunboy83@gmail.com>
2018-07-10 11:29:04 +0900

    - erode

--- image_preprocessing_utils/blur_mask.py
+++ image_preprocessing_utils/blur_mask.py
@@ -2,14 +2,24 @@ import cv2
 import numpy as np
 
 if __name__ == '__main__':
-    img = cv2.imread('./mask.png', cv2.IMREAD_UNCHANGED)
+    img = cv2.imread('./mask_rect.png', cv2.IMREAD_UNCHANGED)
 
     if type(img) is np.ndarray:
 
-        blurred_img = cv2.blur(img, (10, 10))
+        # erosion
+        kernel = np.ones((5, 5), np.uint8)
+        eroded_img = cv2.erode(img, kernel, iterations=1)
 
-        cv2.imshow('original', img)
-        cv2.imshow('result', blurred_img)
+        # averaging
+        blurred_img = cv2.blur(eroded_img, (10, 10))
+
+        #cv2.imshow('original', img)
+        #cv2.imshow('result', blurred_img)
+
+        # stack images vertically
+        vis = np.concatenate((img, eroded_img, blurred_img), axis=0)
+
+        cv2.imshow('results', vis)
 
         cv2.waitKey()
 

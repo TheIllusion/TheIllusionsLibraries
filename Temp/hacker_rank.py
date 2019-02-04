55a878dc39dd0c1185875de950a5031fd7a36123
TheIllusion <gunboy83@gmail.com>
2019-02-05 07:40:49 +0900

    minor

--- Temp/hacker_rank.py
+++ Temp/hacker_rank.py
@@ -1,82 +1,50 @@
 # Enter your code here. Read input from STDIN. Print output to STDOUT
 
-input_file = 'C:/Users/rokky/Desktop/Temp/inputs.txt'
+input_file = 'C:/Users/rokky/Desktop/Temp/input07.txt'
 
 file = open(input_file, 'r')
 
-#test_cases = int(raw_input())
+# Complete the arrayManipulation function below.
+def arrayManipulation(n, queries):
+    lst = [0] * n
+    # print lst
 
-test_cases = int(file.readline().split()[0])
+    print 'len(queries) =', len(queries)
 
-def erase_one_element(current_sidelength, inputs):
-    # print 'inputs =', inputs
+    debug_idx = 0
+    for q in queries:
+        print 'debug_idx =', debug_idx
+        debug_idx += 1
+        '''
+        for i in range(q[0] - 1, q[1]):
+            lst[i] += q[2]
+        '''
 
-    if current_sidelength < max(inputs[0], inputs[len(inputs) - 1]):
-        print 'current_sidelength=', current_sidelength
-        print 'inputs[0], inputs[-1]', inputs[0], inputs[-1]
-        return current_sidelength, inputs
+        lst[q[0]-1] += q[2]
+        if q[1] < n:
+            lst[q[1]] -= q[2]
 
-    updated_sidelength = max(inputs[0], inputs[len(inputs) - 1])
+    max_val = 0
+    val = 0
+    for i in range(n):
+        val = val + lst[i]
+        if val > max_val:
+            max_val = val
 
-    if len(inputs) <= 1:
-        return updated_sidelength, []
+    return max_val
 
-    if inputs[0] > inputs[len(inputs) - 1]:
-        #updated_inputs = inputs[1:]
-        inputs.pop(0)
-    else:
-        #updated_inputs = inputs[:-1]
-        inputs.pop()
+if __name__ == '__main__':
+    nm = file.readline().split()
+    n = int(nm[0])
+    m = int(nm[1])
 
-    '''
-    if inputs[0] > inputs[len(inputs)-1]:
-        updated_sidelength = inputs[0]
-        updated_inputs = inputs[1:]
-    else:
-        updated_sidelength = inputs[len(inputs)-1]
-        updated_inputs = inputs[:-1]
-    '''
+    queries = []
 
-    # print 'updated inputs =', updated_inputs
+    for _ in xrange(m):
+        queries.append(map(int, file.readline().rstrip().split()))
 
-    updated_inputs = inputs
-    return updated_sidelength, updated_inputs
+    result = arrayManipulation(n, queries)
 
-
-for i in range(test_cases):
-    #num = int(raw_input())
-    #print 'i =', i
-
-    num = int(file.readline().split()[0])
-
-    #inputs = map(int, raw_input().split())
-    inputs = map(int, file.readline().split())
-
-    current_sidelength = max(inputs[0], inputs[-1])
-    #print 'start sidelength =', current_sidelength
-    #print 'two values =', inputs[0], inputs[-1]
-
-    if current_sidelength < max(inputs):
-        print 'No'
-        continue
-
-    for j in range(len(inputs)):
-
-        prev_inputs_len = len(inputs)
-        updated_sidelength, updated_inputs = erase_one_element(current_sidelength, inputs)
-
-        if len(updated_inputs) == 0:
-            break
-        if len(updated_inputs) == prev_inputs_len:
-            print 'current inputs size =', len(inputs)
-            print 'No'
-            break
-
-        inputs = updated_inputs
-        current_sidelength = updated_sidelength
-
-    if len(updated_inputs) == 0:
-        print 'Yes'
-    # print inputs
+    print 'result =', result
 
 file.close()

f0a1c78826c40e4aead3dc5c39d56aba48611acb
Illusion <gunboy83@gmail.com>
2019-07-22 16:48:59 +0900

    color pixel dist

--- image_preprocessing_utils/count_pixel_distributions.py
+++ image_preprocessing_utils/count_pixel_distributions.py
@@ -17,14 +17,15 @@ def count_pixel_dist(img):
     counted_dict = Counter(img2)
 
     print counted_dict
+    print 'total pixels =', len(img2)
     print 'num of colors =', len(counted_dict)
 
 
 if __name__ == '__main__':
 
-    #image = cv2.imread('/Users/Illusion/Documents/rk_face.jpg', cv2.IMREAD_COLOR)
+    image = cv2.imread('/Users/Illusion/Temp/30_colors/nicoic-03-148-066-02-f-pjw-160804.jpg_00 copy.png', cv2.IMREAD_COLOR)
     #image = cv2.imread('/Users/Illusion/Temp/yonggu-61-003-172-02-f-whk-181213.jpg', cv2.IMREAD_COLOR)
-    image = cv2.imread('/Users/Illusion/Temp/20_color_yonggu-61-003-172-02-f-whk-181213.png', cv2.IMREAD_COLOR)
+    #image = cv2.imread('/Users/Illusion/Temp/20_color_yonggu-61-003-172-02-f-whk-181213.png', cv2.IMREAD_COLOR)
 
     if type(image) is not np.ndarray:
         print 'cannot read file'

2ec315dffd261f8f0e1efb6cd23ba939271a1a30
Illusion <gunboy83@gmail.com>
2019-08-30 17:37:22 +0900

    add filename modification util

--- Temp/concat_result_images.py
+++ Temp/concat_result_images.py
@@ -10,8 +10,8 @@ import glob
 #OUTPUT_DIRECTORY = "/Users/Illusion/Temp/result_concat/"
 
 INPUT_DIRECTORY = "/Volumes/Ext_850Ev/Comico/nico_ygjm_split_by_mh/grey_split/"
-ANSWER_DIRECTORY = "/Volumes/Ext_850Ev/Comico/nico_ygjm_split_by_mh/64_colors_rk/"
-OUTPUT_DIRECTORY = "/Volumes/Ext_850Ev/Comico/nico_ygjm_split_by_mh/pix2pix_64_colors_rk/"
+ANSWER_DIRECTORY = "/Volumes/Ext_850Ev/Comico/nico_ygjm_split_by_mh/23_colors/"
+OUTPUT_DIRECTORY = "/Volumes/Ext_850Ev/Comico/nico_ygjm_split_by_mh/pix2pix_23_colors/"
 
 if __name__ == "__main__":
 
@@ -31,8 +31,7 @@ if __name__ == "__main__":
             os.system("exit")
             #continue
 
-        png_file = jpg_file[:-4] + '.png'
-        answer_img = cv2.imread( ANSWER_DIRECTORY + png_file, cv2.IMREAD_COLOR)
+        answer_img = cv2.imread( ANSWER_DIRECTORY + jpg_file, cv2.IMREAD_COLOR)
         if (type(answer_img) is not np.ndarray):
             print jpg_file + ' load failed!'
             os.system("exit")
@@ -45,6 +44,8 @@ if __name__ == "__main__":
 
         #try:
         concated_img = np.hstack((input_img, answer_img))
+
+        png_file = jpg_file[:-4] + '.png'
 
         cv2.imwrite(OUTPUT_DIRECTORY + 'concat_' + png_file, concated_img)
         #except:
--- image_preprocessing_utils/count_pixel_distributions.py
+++ image_preprocessing_utils/count_pixel_distributions.py
@@ -23,9 +23,15 @@ if __name__ == '__main__':
 
     #image = cv2.imread('/Users/Illusion/Temp/aaa.jpg', cv2.IMREAD_COLOR)
     #image = cv2.imread('/Users/Illusion/Temp/30_colors/nicoic-05-066-104-04-f-knh-161011.jpg_02.jpg', cv2.IMREAD_COLOR)
-    image = cv2.imread('/Volumes/Ext_850Ev/Comico/nico_ygjm_split_by_mh/pix2pix_full_colors/concat_nicoic-02-023-027-05-f-whk-160417.jpg_04.jpg', cv2.IMREAD_COLOR)
+    #image = cv2.imread('/Volumes/Ext_850Ev/Comico/nico_ygjm_split_by_mh/pix2pix_full_colors/concat_nicoic-02-023-027-05-f-whk-160417.jpg_04.jpg', cv2.IMREAD_COLOR)
     #image = cv2.imread('/Users/Illusion/Temp/yonggu-61-003-172-02-f-whk-181213.jpg', cv2.IMREAD_COLOR)
     #image = cv2.imread('/Users/Illusion/Temp/20_color_yonggu-61-003-172-02-f-whk-181213.png', cv2.IMREAD_COLOR)
+    #image = cv2.imread('/Volumes/Ext_850Ev/Comico/nico_ygjm_split_by_mh/color_split/nicoic-03-149-066-03-f-pjw-160804.jpg_02.jpg')
+    #image = cv2.imread('/Volumes/Ext_850Ev/Comico/nico_ygjm_split_by_mh/pix2pix_23_colors/concat_nicoic-02-026-027-08-f-whk-160524.jpg_00.png')
+    #image = cv2.imread('/Users/Illusion/Temp/concat_nicoic-03-148-066-01-f-pjw-160803.jpg_01.png')
+    #image = cv2.imread('/Users/Illusion/Temp/15_without_background.png')
+    #image = cv2.imread('/Users/Illusion/Temp/26_with_e.png')
+    image = cv2.imread('/Users/Illusion/Temp/small.png')
 
     if type(image) is not np.ndarray:
         print 'cannot read file'

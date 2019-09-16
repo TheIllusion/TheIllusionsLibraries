8677500f647dfa8901c0505beed30b875e1d1164
Illusion <gunboy83@gmail.com>
2019-09-16 18:34:39 +0900

    bilateral filter

--- image_preprocessing_utils/hands/hands_filtering.py
+++ image_preprocessing_utils/hands/hands_filtering.py
@@ -1,28 +1,40 @@
 import cv2
 import numpy as np
 import time
+import os, glob
 
 #HAND_IMG1 = '/Users/Illusion/Temp/rk.jpg'
-ANI_IMG1 = '/Users/Illusion/Temp/epoch166_fake_B.png'
+#ANI_IMG1 = '/Users/Illusion/Temp/epoch166_fake_B.png'
 
+INPUT_DIR = '/Users/Illusion/Documents/Data/palm_data/personal_info_blurring_exp/testset_real/'
+OUTPUT_DIR = '/Users/Illusion/Documents/Data/palm_data/personal_info_blurring_exp/bilateral_output/'
 
 if __name__ == '__main__':
-    img = cv2.imread(ANI_IMG1, cv2.IMREAD_UNCHANGED)
 
-    if type(img) is np.ndarray:
+    if not os.path.exists(OUTPUT_DIR):
+        os.mkdir(OUTPUT_DIR)
 
-        start_time = time.time()
+    img_files = glob.glob(os.path.join(INPUT_DIR, '*.jpg'))
 
-        result = cv2.bilateralFilter(img, d=50, sigmaColor=90, sigmaSpace=80)
+    for img_file in img_files:
+        img = cv2.imread(img_file, cv2.IMREAD_UNCHANGED)
 
-        elapsed_time = time.time() - start_time
+        if type(img) is np.ndarray:
 
-        concated_img = np.hstack((img, result))
+            start_time = time.time()
 
-        cv2.imshow('result', concated_img)
+            result = cv2.bilateralFilter(img, d=50, sigmaColor=90, sigmaSpace=80)
 
-        print 'elapsed_time: ', elapsed_time
+            elapsed_time = time.time() - start_time
 
-        cv2.waitKey()
+            concated_img = np.hstack((img, result))
+
+            #cv2.imshow('result', concated_img)
+
+            print 'elapsed_time: ', elapsed_time
+
+            #cv2.waitKey()
+
+            cv2.imwrite(os.path.join(OUTPUT_DIR, os.path.basename(img_file)), concated_img)
 
     print 'process end'

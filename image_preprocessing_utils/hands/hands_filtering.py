95585631b288348b3a324a7558fd04f48722c4b7
Illusion <gunboy83@gmail.com>
2019-09-17 18:46:55 +0900

    filters for palm

--- image_preprocessing_utils/hands/hands_filtering.py
+++ image_preprocessing_utils/hands/hands_filtering.py
@@ -6,9 +6,13 @@ import os, glob
 #HAND_IMG1 = '/Users/Illusion/Temp/rk.jpg'
 #ANI_IMG1 = '/Users/Illusion/Temp/epoch166_fake_B.png'
 
-INPUT_DIR = '/Users/Illusion/Documents/Data/palm_data/personal_info_blurring_exp/testset_real/'
-OUTPUT_DIR = '/Users/Illusion/Documents/Data/palm_data/personal_info_blurring_exp/multiple_output/'
+#INPUT_DIR = '/Users/Illusion/Documents/Data/palm_data/personal_info_blurring_exp/testset_real/'
 
+INPUT_DIR = '/Users/Illusion/Temp/full_color_pix2pix/'
+OUTPUT_DIR = '/Users/Illusion/Temp/full_color_pix2pix_multiple_output/'
+
+#INPUT_DIR = '/Users/Illusion/Temp/ex_color_pix2pix/'
+#OUTPUT_DIR = '/Users/Illusion/Temp/ex_color_pix2pix_multiple_output/'
 
 def bilateral_filter(img):
     bilateral_result = cv2.bilateralFilter(img, d=50, sigmaColor=90, sigmaSpace=80)
@@ -20,17 +24,24 @@ def guided_filter(img):
     guided_result = cv2.ximgproc.guidedFilter(img, img, 50, 50)
     return guided_result
 
+
 if __name__ == '__main__':
 
     if not os.path.exists(OUTPUT_DIR):
         os.mkdir(OUTPUT_DIR)
 
-    img_files = glob.glob(os.path.join(INPUT_DIR, '*.jpg'))
+    img_files = glob.glob(os.path.join(INPUT_DIR, '*.png'))
 
     for img_file in img_files:
         img = cv2.imread(img_file, cv2.IMREAD_UNCHANGED)
 
         if type(img) is np.ndarray:
+
+            # edge-preserving (opencv v4)
+            start_time = time.time()
+            edge_preserving_result = cv2.ximgproc.edgePreservingFilter(img, d=40, threshold=15)
+            elapsed_time = time.time() - start_time
+            print 'Edge-preserving filter elapsed_time: ', elapsed_time
 
             # Bilateral
             start_time = time.time()
@@ -46,24 +57,26 @@ if __name__ == '__main__':
             '''
 
             # L0-smoothing
+            l0_lambda = 0.005
+            l0_kappa = 1.5
             start_time = time.time()
-            l0_result = cv2.ximgproc.l0Smooth(img, None, 0.012, 2.0)
+            l0_result = cv2.ximgproc.l0Smooth(img, None, l0_lambda, l0_kappa)
             elapsed_time = time.time() - start_time
             print 'L0 filter elapsed_time: ', elapsed_time
 
             # Bilateral + L0-smoothing
             start_time = time.time()
-            bi_and_l0_result = cv2.ximgproc.l0Smooth(bilateral_result, None, 0.012, 2.0)
+            bi_and_l0_result = cv2.ximgproc.l0Smooth(bilateral_result, None, l0_lambda, l0_kappa)
             elapsed_time = time.time() - start_time
             print 'L0(input:bilateral) filter elapsed_time: ', elapsed_time
 
-            concated_img = np.hstack((img, bilateral_result, l0_result, bi_and_l0_result))
+            concated_img = np.hstack((img, edge_preserving_result, bilateral_result, l0_result, bi_and_l0_result))
 
-            #cv2.imshow('result', concated_img)
-            #cv2.waitKey()
+            cv2.imshow('result', concated_img)
+            cv2.waitKey()
 
-            cv2.imwrite(os.path.join(OUTPUT_DIR, os.path.basename(img_file)), concated_img)
+            #cv2.imwrite(os.path.join(OUTPUT_DIR, os.path.basename(img_file)), concated_img)
 
-        #break
+        break
 
     print 'process end'

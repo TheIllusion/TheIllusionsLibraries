912a306dc4b2da826d91be3836f5e4280d2f1a68
Illusion <gunboy83@gmail.com>
2019-09-18 15:04:20 +0900

    filters for palm

--- image_preprocessing_utils/hands/hands_filtering.py
+++ image_preprocessing_utils/hands/hands_filtering.py
@@ -6,23 +6,23 @@ import os, glob
 #HAND_IMG1 = '/Users/Illusion/Temp/rk.jpg'
 #ANI_IMG1 = '/Users/Illusion/Temp/epoch166_fake_B.png'
 
-#INPUT_DIR = '/Users/Illusion/Documents/Data/palm_data/personal_info_blurring_exp/testset_real/'
+INPUT_DIR = '/Users/Illusion/Documents/Data/palm_data/personal_info_blurring_exp/testset_real/'
+OUTPUT_DIR = '/Users/Illusion/Documents/Data/palm_data/personal_info_blurring_exp/multiple_output/'
 
-INPUT_DIR = '/Users/Illusion/Temp/full_color_pix2pix/'
-OUTPUT_DIR = '/Users/Illusion/Temp/full_color_pix2pix_multiple_output/'
+#INPUT_DIR = '/Users/Illusion/Temp/full_color_pix2pix/'
+#OUTPUT_DIR = '/Users/Illusion/Temp/full_color_pix2pix_multiple_output/'
 
 #INPUT_DIR = '/Users/Illusion/Temp/ex_color_pix2pix/'
 #OUTPUT_DIR = '/Users/Illusion/Temp/ex_color_pix2pix_multiple_output/'
 
-def bilateral_filter(img):
-    bilateral_result = cv2.bilateralFilter(img, d=50, sigmaColor=90, sigmaSpace=80)
-    return bilateral_result
+SINGLE_IMG_VIEW_FLAG = False
 
-
-# Kaiming He
-def guided_filter(img):
-    guided_result = cv2.ximgproc.guidedFilter(img, img, 50, 50)
-    return guided_result
+# FILTER LIST
+EDGE_PRESERVING = False
+GUIDED = False
+BILATERAL = True
+L0_SMOOTHING = False
+BI_AND_LO = False
 
 
 if __name__ == '__main__':
@@ -30,53 +30,108 @@ if __name__ == '__main__':
     if not os.path.exists(OUTPUT_DIR):
         os.mkdir(OUTPUT_DIR)
 
-    img_files = glob.glob(os.path.join(INPUT_DIR, '*.png'))
+    img_files = glob.glob(os.path.join(INPUT_DIR, '*.jpg'))
+
+    idx = 0
 
     for img_file in img_files:
+
+        print '---------- img idx: ', idx + 1
+        idx += 1
+
         img = cv2.imread(img_file, cv2.IMREAD_UNCHANGED)
 
         if type(img) is np.ndarray:
 
+            result_imgs = []
+            result_imgs.append(img)
+
             # edge-preserving (opencv v4)
-            start_time = time.time()
-            edge_preserving_result = cv2.ximgproc.edgePreservingFilter(img, d=40, threshold=15)
-            elapsed_time = time.time() - start_time
-            print 'Edge-preserving filter elapsed_time: ', elapsed_time
+            if EDGE_PRESERVING:
+                start_time = time.time()
+                # ximgproc::edgePreservingFilter(image, res, 9, 20);
+                edge_preserving_result = cv2.ximgproc.edgePreservingFilter(img, d=9, threshold=10)
+                result_imgs.append(edge_preserving_result)
+                elapsed_time = time.time() - start_time
+                print 'Edge-preserving filter elapsed_time: ', elapsed_time
 
-            # Bilateral
-            start_time = time.time()
-            bilateral_result = bilateral_filter(img)
-            elapsed_time = time.time() - start_time
-            print 'Bilateral filter elapsed_time: ', elapsed_time
+            if GUIDED:
+                # Guided (Kaiming He)
+                start_time = time.time()
+                guided_result = cv2.ximgproc.guidedFilter(img.copy(), img, 8, 500)
+                result_imgs.append(guided_result)
+                elapsed_time = time.time() - start_time
+                print 'Guided filter elapsed_time: ', elapsed_time
 
-            '''
-            start_time = time.time()
-            guided_result = guided_filter(img)
-            elapsed_time = time.time() - start_time
-            print 'Guided filter elapsed_time: ', elapsed_time
-            '''
+            if BILATERAL:
+                # Bilateral
+                start_time = time.time()
+                bilateral_result = cv2.bilateralFilter(img, d=60, sigmaColor=90, sigmaSpace=40)
+                result_imgs.append(bilateral_result)
+                elapsed_time = time.time() - start_time
+                print 'Bilateral filter elapsed_time: ', elapsed_time
 
-            # L0-smoothing
-            l0_lambda = 0.005
-            l0_kappa = 1.5
-            start_time = time.time()
-            l0_result = cv2.ximgproc.l0Smooth(img, None, l0_lambda, l0_kappa)
-            elapsed_time = time.time() - start_time
-            print 'L0 filter elapsed_time: ', elapsed_time
+                # stronger
+                start_time = time.time()
+                bilateral_result = cv2.bilateralFilter(img, d=60, sigmaColor=150, sigmaSpace=40)
+                result_imgs.append(bilateral_result)
+                elapsed_time = time.time() - start_time
+                print 'Bilateral filter elapsed_time: ', elapsed_time
 
-            # Bilateral + L0-smoothing
-            start_time = time.time()
-            bi_and_l0_result = cv2.ximgproc.l0Smooth(bilateral_result, None, l0_lambda, l0_kappa)
-            elapsed_time = time.time() - start_time
-            print 'L0(input:bilateral) filter elapsed_time: ', elapsed_time
+                # stronger (using resize)
+                # start_time = time.time()
+                # h, w, c = img.shape
+                # resized_img = cv2.resize(img, (h / 2, w / 2), interpolation=cv2.INTER_CUBIC)
+                # bilateral_result = cv2.bilateralFilter(resized_img, d=30, sigmaColor=150, sigmaSpace=40)
+                # bilateral_result = cv2.resize(bilateral_result, (h, w), interpolation=cv2.INTER_CUBIC)
+                # result_imgs.append(bilateral_result)
+                # elapsed_time = time.time() - start_time
+                # print 'Bilateral filter elapsed_time: ', elapsed_time
 
-            concated_img = np.hstack((img, edge_preserving_result, bilateral_result, l0_result, bi_and_l0_result))
+                # stronger
+                start_time = time.time()
+                bilateral_result = cv2.bilateralFilter(img, d=60, sigmaColor=220, sigmaSpace=40)
+                result_imgs.append(bilateral_result)
+                elapsed_time = time.time() - start_time
+                print 'Bilateral filter elapsed_time: ', elapsed_time
 
-            cv2.imshow('result', concated_img)
-            cv2.waitKey()
+                # stronger (using resize)
+                # start_time = time.time()
+                # h, w, c = img.shape
+                # resized_img = cv2.resize(img, (h/2, w/2), interpolation=cv2.INTER_CUBIC)
+                # bilateral_result = cv2.bilateralFilter(resized_img, d=30, sigmaColor=220, sigmaSpace=40)
+                # bilateral_result = cv2.resize(bilateral_result, (h, w), interpolation=cv2.INTER_CUBIC)
+                # result_imgs.append(bilateral_result)
+                # elapsed_time = time.time() - start_time
+                # print 'Bilateral filter elapsed_time: ', elapsed_time
 
-            #cv2.imwrite(os.path.join(OUTPUT_DIR, os.path.basename(img_file)), concated_img)
+            if L0_SMOOTHING:
+                # L0-smoothing
+                l0_lambda = 0.005
+                l0_kappa = 1.5
+                start_time = time.time()
+                l0_result = cv2.ximgproc.l0Smooth(img, None, l0_lambda, l0_kappa)
+                result_imgs.append(l0_result)
+                elapsed_time = time.time() - start_time
+                print 'L0 filter elapsed_time: ', elapsed_time
 
-        break
+            if BI_AND_LO:
+                # Bilateral + L0-smoothing
+                start_time = time.time()
+                bi_and_l0_result = cv2.ximgproc.l0Smooth(bilateral_result, None, l0_lambda, l0_kappa)
+                result_imgs.append(bi_and_l0_result)
+                elapsed_time = time.time() - start_time
+                print 'L0(input:bilateral) filter elapsed_time: ', elapsed_time
+
+            concat_img = np.hstack(tuple(result_imgs))
+
+            if SINGLE_IMG_VIEW_FLAG:
+                cv2.imshow('result', concat_img)
+                cv2.waitKey()
+            else:
+                cv2.imwrite(os.path.join(OUTPUT_DIR, os.path.basename(img_file)), concat_img)
+
+        if SINGLE_IMG_VIEW_FLAG:
+            break
 
     print 'process end'

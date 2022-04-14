0adf5a0a5d4c2e4de41faac6fcc75700104c2b53
TheIllusion <gunboy83@gmail.com>
2022-04-14 10:08:26 +0900

    - server monitoring - add m1 pro macbook settings

--- monitoring/urgent_monitoring_with_rest_api.py
+++ monitoring/urgent_monitoring_with_rest_api.py
@@ -1,22 +1,32 @@
 import time
 import os
-import urllib2
 import datetime
 import re
 import sys
+
+# python2
+import urllib2
+
+# python3
+#import urllib.request as urllib2
 
 REQUEST_SENDING_INTERVAL = 0.1
 REQUEST_TIMEOUT_THRESHOLD_SEC = 30
 RESPONSE_TIME_WARINING_THRESHOLD = 5
 
-FILE_PATH_FOR_ALL_LOG = '/Users/Illusion/Temp/response_all_log.txt'
-FILE_PATH_FOR_WARNING_LOG = '/Users/Illusion/Temp/response_warning.log'
-FILE_PATH_FOR_ERROR_LOG = '/Users/Illusion/Temp/response_error.log'
+#FILE_PATH_FOR_ALL_LOG = '/Users/Illusion/Temp/response_all_log.txt'
+#FILE_PATH_FOR_WARNING_LOG = '/Users/Illusion/Temp/response_warning.log'
+#FILE_PATH_FOR_ERROR_LOG = '/Users/Illusion/Temp/response_error.log'
 
-#Macbook Pro 16'
+#Macbook Pro 16' (Intel)
 #FILE_PATH_FOR_ALL_LOG = '/Users/rokkyulee/Temp/response_all_log.txt'
 #FILE_PATH_FOR_WARNING_LOG = '/Users/rokkyulee/Temp/response_warning.log'
 #FILE_PATH_FOR_ERROR_LOG = '/Users/rokkyulee/Temp/response_error.log'
+
+#Macbook Pro 16' (M1 pro)
+FILE_PATH_FOR_ALL_LOG = '/Users/illusion-m1pro/Temp/response_all_log.txt'
+FILE_PATH_FOR_WARNING_LOG = '/Users/illusion-m1pro/Temp/response_warning.log'
+FILE_PATH_FOR_ERROR_LOG = '/Users/illusion-m1pro/Temp/response_error.log'
 
 #rest_api_address = 'http://vincent.nhnent.com:8979/hand'
 #hand_rest_api_address = 'http://10.165.128.51:8979/hand'
@@ -30,23 +40,24 @@ hand_rest_api_address.append('http://10.161.31.26:8979/hand-v2')
 hand_rest_api_address.append('http://10.161.31.27:8979/hand-v2')
 
 #face_rest_api_address.append('http://10.165.128.51:8989/face-v2')
-face_rest_api_address.append('http://10.161.31.22:8989/face-v2')
+#face_rest_api_address.append('http://10.161.31.22:8989/face-v2')
 face_rest_api_address.append('http://10.161.31.26:8989/face-v2')
 face_rest_api_address.append('http://10.161.31.27:8989/face-v2')
 
 #hand_img_path = '/Users/Illusion/Documents/Palm_Data/random_hands/IMG_0703.jpg'
 
 #12' Macbook
-hand_img_path = '/Users/Illusion/Documents/rk_hand.jpg'
-face_img_path = '/Users/Illusion/Documents/rk_face.jpg'
+#hand_img_path = '/Users/Illusion/Documents/rk_hand.jpg'
+#face_img_path = '/Users/Illusion/Documents/rk_face.jpg'
 
-#Macbook Pro 16'
+#Macbook Pro 16' (M1 pro)
+hand_img_path = '/Users/illusion-m1pro/Documents/rk_hand.jpg'
+face_img_path = '/Users/illusion-m1pro/Documents/rk_face.jpg'
+
+#Macbook Pro 16' (Intel)
 #hand_img_path = '/Users/rokkyulee/rk_hand.jpg'
 #face_img_path = '/Users/rokkyulee/rk_face.jpg'
 
-#Macbook Pro 13'
-#hand_img_path = '/Users/Illusion/Documents/Data/palm_data/test_set/crop_resize_512_512/rk.jpg'
-#face_img_path = '/Users/Illusion/Documents/Data/toast_faces/rk.jpg'
 
 f_hand = open(hand_img_path, 'rb')
 hand_image = f_hand.read()
@@ -61,15 +72,15 @@ index = 0
 hand_server_num = len(hand_rest_api_address)
 face_server_num = len(face_rest_api_address)
 
-print 'Total number of hand servers = ', str(hand_server_num)
-print 'Total number of face servers = ', str(face_server_num)
+print('Total number of hand servers = ', str(hand_server_num))
+print('Total number of face servers = ', str(face_server_num))
 while True:
 
     ###########################################################################
     # Check response time for hand recognition
 
-    print '==================================================================='
-    print 'hand ip = ', hand_rest_api_address[index]
+    print('===================================================================')
+    print('hand ip = ', hand_rest_api_address[index])
 
     try:
         start_time = time.time()
@@ -85,7 +96,7 @@ while True:
         current_time = datetime.datetime.strftime(datetime.datetime.now(), '%Y-%m-%d %H:%M:%S')
 
         line_string_all_log = current_time + ' Elapsed time(hand) = ' + str(elapsed_time) + '\n'
-        print line_string_all_log
+        print(line_string_all_log)
 
         all_log_file = open(FILE_PATH_FOR_ALL_LOG, 'a')
         all_log_file.write(line_string_all_log)
@@ -97,7 +108,7 @@ while True:
             line_string = current_time + ' Warning!!!!! Response time for hand recognition is slower than ' \
                           + str(RESPONSE_TIME_WARINING_THRESHOLD) + 's. Response Time = ' + str(elapsed_time) + hand_rest_api_address[index] + '\n'
 
-            print line_string
+            print(line_string)
             warning_log_file.write(line_string)
             warning_log_file.close()
 
@@ -105,14 +116,14 @@ while True:
         match = re.search('"Status": "OK"', res)
         match_2 = re.search('"Gamjeong": 2000', res)
         if match and match_2:
-            print "Valid response data"
+            print("Valid response data")
         else:
-            print "Invalid response data"
+            print("Invalid response data")
             warning_log_file = open(FILE_PATH_FOR_WARNING_LOG, 'a')
 
             line_string = current_time + ' Warning!!!!! Invalid response detected from the hand response' + hand_rest_api_address[index] + '\n'
 
-            print line_string
+            print(line_string)
             warning_log_file.write(line_string)
 
             warning_log_file.close()
@@ -125,11 +136,11 @@ while True:
         current_time = datetime.datetime.strftime(datetime.datetime.now(), '%Y-%m-%d %H:%M:%S')
         line_string = current_time + ' Error!! Exception occurred in hand! ' + hand_rest_api_address[index] + '\n'
 
-        print line_string
+        print(line_string)
         warning_log_file.write(line_string)
 
         exception_string = "Unexpected error:" + str(sys.exc_info()[0]) + '\n'
-        print exception_string
+        print(exception_string)
         warning_log_file.write(exception_string)
 
         warning_log_file.close()
@@ -139,8 +150,8 @@ while True:
     ###########################################################################
     # Check response time for face recognition
 
-    print '==================================================================='
-    print 'face ip = ', face_rest_api_address[index]
+    print('===================================================================')
+    print('face ip = ', face_rest_api_address[index])
 
     try:
         req = urllib2.Request(face_rest_api_address[index], data=face_image)
@@ -156,7 +167,7 @@ while True:
         current_time = datetime.datetime.strftime(datetime.datetime.now(), '%Y-%m-%d %H:%M:%S')
 
         line_string = current_time + ' Elapsed time(face) = ' + str(elapsed_time) + '\n'
-        print line_string
+        print(line_string)
 
         all_log_file = open(FILE_PATH_FOR_ALL_LOG, 'a')
         all_log_file.write(line_string)
@@ -168,8 +179,8 @@ while True:
             line_string = current_time + ' Warning!!!!! Response time for face recognition is slower than ' \
                           + str(RESPONSE_TIME_WARINING_THRESHOLD) + 's. Response Time = ' + str(elapsed_time) + face_rest_api_address[index] + '\n'
 
-            print line_string
-            print '!!!!===================================================================!!!!'
+            print(line_string)
+            print('!!!!===================================================================!!!!')
 
             warning_log_file.write(line_string)
             warning_log_file.close()
@@ -178,14 +189,14 @@ while True:
         match = re.search('"Status": "OK"', res)
         match_2 = re.search('"eyebrows": 1010', res)
         if match and match_2:
-            print "Valid response data"
+            print("Valid response data")
         else:
-            print "Invalid response data"
+            print("Invalid response data")
             warning_log_file = open(FILE_PATH_FOR_WARNING_LOG, 'a')
 
             line_string = current_time + ' Warning!!!!! Invalid response detected from the face response' + face_rest_api_address[index] + '\n'
-            print '!!!!===================================================================!!!!'
-            print line_string
+            print('!!!!===================================================================!!!!')
+            print(line_string)
             warning_log_file.write(line_string)
             warning_log_file.close()
 
@@ -197,13 +208,13 @@ while True:
         current_time = datetime.datetime.strftime(datetime.datetime.now(), '%Y-%m-%d %H:%M:%S')
         line_string = current_time + ' Error!! Exception Occurred in face! ' + face_rest_api_address[index] + '\n'
 
-        print line_string
-        print '!!!!===================================================================!!!!'
+        print(line_string)
+        print('!!!!===================================================================!!!!')
 
         warning_log_file.write(line_string)
 
         exception_string = "Unexpected error:" + str(sys.exc_info()[0]) + '\n'
-        print exception_string
+        print(exception_string)
         warning_log_file.write(exception_string)
 
         warning_log_file.close()
